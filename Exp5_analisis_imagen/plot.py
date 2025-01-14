c0f32963ed625ddc4912d0a893d82c506d710c4a
Andrés Pinzón <apinzonh@unal.edu.co>
2025-01-14 16:47:14 -0500

    Práctica 5. Gráfica final generada para 5a.

--- Exp5_analisis_imagen/plot.py
+++ Exp5_analisis_imagen/plot.py
@@ -4,10 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 # Leer el archivo Excel
-df = pd.read_excel("roiStats.xlsx")
-# df = pd.read_excel("pixelIrradiance.xlsx")
+df = pd.read_excel("Exp5_analisis_imagen/roiStats.xlsx")
+# df = pd.read_excel("Exp5_analisis_imagen/pixelIrradiance.xlsx")
 
 # Extraer columnas como arrays de NumPy
 # errors = [df["Rstd"].to_numpy()]
@@ -22,12 +21,30 @@ fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
 
 # Formato de las gráficas
 format_plot = dict(marker="o", ls="-", lw="0.5", fillstyle="none")
-colors = {'red':"#FF0000", 'green':"#00FF00", 'blue':"#0000FF"}
+colors = {"red": "#FF0000", "green": "#00FF00", "blue": "#0000FF"}
 
 # Gráfica de los puntos con barras de error radiales
-ax.plot(angles, df["Rmean"].to_numpy(), label="Rojo", color=colors['red'], **format_plot)
-ax.plot(angles, df["Gmean"].to_numpy(), label="Verde", color=colors['green'], **format_plot)
-ax.plot(angles, df["Bmean"].to_numpy(), label="Azul", color=colors['blue'], **format_plot)
+ax.plot(
+    angles,
+    df["Rmean"].to_numpy(),
+    label="Irradiancia Roja",
+    color=colors["red"],
+    **format_plot
+)
+ax.plot(
+    angles,
+    df["Gmean"].to_numpy(),
+    label="Irradiancia Verde",
+    color=colors["green"],
+    **format_plot
+)
+ax.plot(
+    angles,
+    df["Bmean"].to_numpy(),
+    label="Irradiancia Azul",
+    color=colors["blue"],
+    **format_plot
+)
 
 # ax.errorbar(
 #     angles,
@@ -41,9 +58,11 @@ ax.plot(angles, df["Bmean"].to_numpy(), label="Azul", color=colors['blue'], **fo
 # )
 
 # Personalización del gráfico
-ax.set_title("Gráfica Polar con Incertidumbres", va="bottom")
-ax.set_rlabel_position(90)
+# ax.set_title("Ley de Malus", va="bottom")
+ax.set_rlabel_position(95)
+ax.set_rticks(np.arange(0, 0.5, 0.1))  # Less radial ticks
 ax.legend()
 
 # Mostrar la gráfica
+fig.savefig("Exp5_analisis_imagen/plot_malus_a.png", dpi=300)
 plt.show()
